7bfbffd245945e26320d909e08a7e1188707fe58
Linda ONeill <oneill.linda21@gmail.com>
2017-11-29 20:45:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,8 +16,7 @@ def play(board)
      turn(board)
    end
    if won?(board)
-     winner(board) == "X" || winner(board) == "O"
-       puts "Congratulations #{winner(board)}!"
+     puts "Congratulations #{winner(board)}!"
    elsif draw?(board)
      puts "Cat's Game!"
    end
